0b86b2eda63611ecdf85a6d50d5b2e0cb988450f
DenisAfa <denisafanasevm92@mail.ru>
2020-12-14 21:48:07 +0300

    feat: add functions closures

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -158,8 +158,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function usingArg(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 
@@ -180,8 +182,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom - 1;
+  return function generator() {
+    id += 1;
+    return id;
+  };
 }
 
 
